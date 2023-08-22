d5365d67bdebde22e7d36d74af52b470e58aa0ee
sadeem74 <sadeem.faisal@outlook.com>
2023-08-22 16:40:23 +0100

    update

--- main2.js
+++ main2.js
@@ -9,7 +9,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg2'),
+  canvas: document.querySelector('bg'),
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
